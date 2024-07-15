f81b944ce98d0415d96f020c5cd7e98121887540
madelds <sibalmadel@gmail.com>
2024-07-15 11:52:03 -0700

    Added 'Log Out' Button

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,25 @@
 import React from 'react'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import dashboardHeader from './dashboardHeader';
+import './App.css';
 
 const App = () => {
   return (
+  <>
     <div>Hi from the frontend!</div>
+
+    // Routing for buttons
+    <Router>
+    <div className="App">
+      <dashboardHeader />
+      <Routes>
+      // Routes "landingPage" to landingPage
+      <Route path="/landingPage" element={<landingPage />} />
+      </Routes>
+    </div>
+    </Router>
+  </>
   )
 }
 
-export default App
+export default App;
---
+++ src/Components/dashboardHeader/dashboardHeader.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { Link } from 'react-router-dom';
+import "./dashboardHeader.css";
+
+const dashboardHeader = () => {
+    <div className="dashboardHeaderBox">
+        <h1 className="dashboardHeaderText">Dashboard</h1>
+        // Routes logOutButton to landingPage
+        <Link to="/landingPage">
+            <button className="logOutButton">Log Out</button>
+        </Link>
+    </div>
+}
+
